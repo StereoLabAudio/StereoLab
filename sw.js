1592f9d62f8c173e7c4fd7531c493cc0cf0bb2cb
StereoLabAudio <jpcohenpro@gmail.com>
2026-08-18 15:12:04 +0200

    Add files via upload
    Stereolab V 4.4.1 clair

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'stereolab-v4.4.1';
+const CACHE_NAME = 'stereolab-v4.4.1-light-test';
 const ASSETS = [
   './',
   './index.html',
@@ -25,11 +25,9 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-
   event.respondWith(
     caches.match(event.request).then(cached => {
       if (cached) return cached;
-
       return fetch(event.request).then(response => {
         const copy = response.clone();
         caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
